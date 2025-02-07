e0f0c36d9968bb4cf1f5cab6755bc7e7ff7ef670
Mahfuj Mahtab Mohot <mohotmohot1@gmail.com>
2025-02-07 22:31:53 +0600

    working on banner

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div className='max-w-full h-auto'>
+        <div className='w-384 m-auto h-125 grid grid-cols-2'>
+            <div className='w-75 h-125 bg-indigo-500'></div>
+            <div className='w-305 h-125 bg-amber-200'></div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Banner
